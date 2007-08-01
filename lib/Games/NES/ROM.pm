40a9f7ce8b66434952da600be0dd71573e64cee9
Brian Cassidy <bricas@cpan.org>
2007-08-01 14:43:53 +0000

    fix up install instructions

--- lib/Games/NES/ROM.pm
+++ lib/Games/NES/ROM.pm
@@ -42,15 +42,6 @@ ROM file is layed out as follows:
 
 =head1 INSTALLATION
 
-To install this module via Module::Build:
-
-    perl Build.PL
-    ./Build         # or `perl Build`
-    ./Build test    # or `perl Build test`
-    ./Build install # or `perl Build install`
-
-To install this module via ExtUtils::MakeMaker:
-
     perl Makefile.PL
     make
     make test
